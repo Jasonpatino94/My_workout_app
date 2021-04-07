464ed4d7935a57836388c237e44f7cbdf2edc23d
Jason <Jason.patino94@gmail.com>
2021-04-07 11:47:23 -0400

    fix errors

--- frontend/src/components/workoutComponents/WorkoutList.js
+++ frontend/src/components/workoutComponents/WorkoutList.js
@@ -6,10 +6,12 @@ const ShowWorkouts = () => {
 	const state = useSelector((state) => state);
 
 	const renderWorkouts = state.workouts.map((workout) => (
-		<p className="workouts">
-			{workout.name} on {new Date(workout.created_at).toLocaleDateString()}
+		<div className="workouts" key={workout.id}>
+			<p>
+				{workout.name} on {new Date(workout.created_at).toLocaleDateString()}
+			</p>
 			<WorkoutModal workout={workout} />
-		</p>
+		</div>
 	));
 
 	return (
