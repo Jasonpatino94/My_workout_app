5335fcf5970ab7ee2c1d260fb9705e379471e53d
Jason <Jason.patino94@gmail.com>
2021-05-17 08:16:28 -0400

    fixed workoutSessionReducer

--- frontend/src/redux/reducers/workoutSessionReducer.js
+++ frontend/src/redux/reducers/workoutSessionReducer.js
@@ -1,8 +1,8 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default (state = [], action) => {
 	switch (action.type) {
-		// case "SET_USER":
-		// 	return action.payload.workouts_sessions;
+		case "SET_USER":
+			return action.payload.workout_sessions || state;
 		case "ADD_WORKOUT_SESSION":
 			return [...state, action.payload];
 		case "DELETE_WORKOUT_SESSION":
